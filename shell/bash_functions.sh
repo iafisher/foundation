0f3293e0da819cc6a350c298ba8c751315b4c916
Ian Fisher <ian@iafisher.com>
2025-08-16 15:43:50 -0400

    shell/bash_functions.sh: arg checks

--- shell/bash_functions.sh
+++ shell/bash_functions.sh
@@ -1,3 +1,31 @@
 status() {
   echo "==> $1"
 }
+
+__assert_zero_args() {
+  if ! [[ $# -eq 0 ]]; then
+    echo "error: command takes 0 arguments"
+    return 1
+  fi
+}
+
+__assert_one_arg() {
+  if ! [[ $# -eq 1 ]]; then
+    echo "error: command takes 1 argument"
+    return 1
+  fi
+}
+
+__assert_zero_or_one_arg() {
+  if [[ $# -gt 1 ]] || [[ "$1" = -* ]]; then
+    echo "error: command takes 0-1 arguments"
+    return 1
+  fi
+}
+
+__assert_one_or_more_args() {
+  if [[ $# -eq 0 ]]; then
+    echo "error: command takes 1 or more arguments"
+    return 1
+  fi
+}
