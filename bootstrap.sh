8de834f03c732bc8308d001866f1c548f21f4311
Ian Fisher <ian@iafisher.com>
2025-08-23 13:46:46 -0400

    improve bootstraph.sh script

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,27 +3,57 @@
 set -eu
 
 main() {
-  if [[ $# -ne 0 ]]; then
-    echo "bootstrap.sh takes no arguments."
+  if [[ $# -ne 1 ]]; then
+    echo "Error: You must pass exactly one argument, the directory to create the 'dotfiles' repo in."
     exit 1
   fi
+
+  code_dir="$1"
+  dotfiles_dir="$code_dir/dotfiles"
 
   if ! command -v git &>/dev/null; then
     echo "git must be installed."
     exit 1
   fi
 
+  echo "==> creating $HOME/.ian and subdirectories"
   mkdir -p "$HOME/.ian"
   mkdir -p "$HOME/.ian/bin"
   mkdir -p "$HOME/.ian/pythonpath"
 
+  echo "==> cloning 'foundation' repo"
   foundation_dir="$HOME/.ian/foundation"
   git clone 'https://github.com/iafisher/foundation.git' "$foundation_dir"
+
+  echo "==> setting up local 'dotfiles' repo"
+  mkdir -p "$dotfiles_dir"
+  cd "$dotfiles_dir"
+  git init
+
+  cp "$foundation_dir/assets/gitignore" .gitignore
+  git add .gitignore
+
+  if [[ "$SHELL" = *zsh ]]; then
+    shellrc="zshrc"
+  else
+    shellrc="bashrc"
+  fi
+
+  echo "# This file contains machine-specific configuration." > "$shellrc"
+  git add "$shellrc"
+
+  git config --global user.name 'Ian Fisher'
+  git config --global user.email 'ian@iafisher.com'
+  git commit -m "initial commit" --author 'Ian Fisher <ian@iafisher.com>'
+
+  git clone . "$HOME/.ian/dotfiles"
+  git config --local receive.denyCurrentBranch updateInstead
 
   echo
   echo "Please add the following line to your shell config:"
   echo
-  echo "  source \$HOME/.ian/foundation/shell/env"
+  echo "  source \"$HOME/.ian/foundation/shell/env\""
+  echo "  source \"$dotfiles_dir/$shellrc\""
   echo
 }
 
--- shell/bash_functions.sh
+++ shell/bash_functions.sh
@@ -1,31 +1,55 @@
+red() {
+  echo "\033[31m$@\033[0m"
+}
+
+yellow() {
+  echo "\033[33m$@\033[0m"
+}
+
+cyan() {
+  echo "\033[36m$@\033[0m"
+}
+
+green() {
+  echo "\033[32m$@\033[0m"
+}
+
+gray() {
+  echo "\033[90m$@\033[0m"
+}
+
+error() {
+  echo "$(red error:) $@" >&2
+}
+
 status() {
-  echo "==> $1"
+  echo "$(gray ==>) $1"
 }
 
 __assert_zero_args() {
   if ! [[ $# -eq 0 ]]; then
-    echo "error: command takes 0 arguments"
+    error "command takes 0 arguments"
     return 1
   fi
 }
 
 __assert_one_arg() {
   if ! [[ $# -eq 1 ]]; then
-    echo "error: command takes 1 argument"
+    error "command takes 1 argument"
     return 1
   fi
 }
 
 __assert_zero_or_one_arg() {
   if [[ $# -gt 1 ]] || [[ "$1" = -* ]]; then
-    echo "error: command takes 0-1 arguments"
+    error "command takes 0 or 1 argument"
     return 1
   fi
 }
 
 __assert_one_or_more_args() {
   if [[ $# -eq 0 ]]; then
-    echo "error: command takes 1 or more arguments"
+    error "command takes 1 or more arguments"
     return 1
   fi
 }
