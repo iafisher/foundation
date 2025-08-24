985f749095237c4e4d3427afe8edaf6ab38d0621
Ian Fisher <ian@iafisher.com>
2025-08-24 08:48:46 -0400

    ian_parse_flags_subcmd function

--- shell/bash_functions.sh
+++ shell/bash_functions.sh
@@ -28,10 +28,10 @@ fatal() {
 }
 
 status() {
-  echo "$(gray '==>') $1"
+  echo "==> $@"
 }
 
-# TODO(2025-08): Replace uses of these functions with `__parse_flags`.
+# TODO(2025-08): Replace uses of these functions with `ian_parse_flags`.
 __assert_zero_args() {
   if ! [[ $# -eq 0 ]]; then
     error "command takes 0 arguments"
@@ -62,7 +62,7 @@ __assert_one_or_more_args() {
 
 ### Example usage:
 ###
-###     __parse_flags 'PATH [--name=] --verbose' "$@"
+###     ian_parse_flags 'PATH [--name=] --verbose' "$@"
 ###     path="${__args[PATH]}"
 ###     name="${__args[--name]}"
 ###     verbose="${__args[--verbose]}"
@@ -70,20 +70,18 @@ __assert_one_or_more_args() {
 ### `PATH` is a positional argument. `[--name=]` is an optional flag that takes an argument.
 ### `--verbose` is a switch (a flag that takes no argument).
 declare -A __args
-__parse_flags() {
-  if [[ $- == *u* ]]; then
-    restore_nounset="1"
-  else
-    restore_nounset="0"
+ian_parse_flags() {
+  if (( $# == 0 )); then
+    echo "BUG: ian_parse_flags takes at least one argument"
+    exit 1
   fi
-  # We need to do potentially unbound hash-map lookups, so this has to be turned off.
-  set +u
 
   desc="$1"
   shift
 
+  cmd="${IAN_PARSE_FLAGS_CMD:-$0}"
   __usage_error() {
-    echo "usage: $0 $desc"
+    echo "usage: $cmd $desc"
     echo
     echo "error: $1"
     exit 1
@@ -123,29 +121,29 @@ __parse_flags() {
     arg="$1"
     case "$arg" in
       -h|-help|--help)
-        echo "usage: $0 $desc"
+        echo "usage: $cmd $desc"
         exit 0
         ;;
       -*)
-        b_rf="${required_flags[$arg]}"
-        b_of="${optional_flags[$arg]}"
-        b_sw="${known_switches[$arg]}"
-        if [[ -n "$b_rf" ]]; then
-          (( b_rf == 1 )) && __usage_error "flag: $arg was repeated"
+        if [[ -v required_flags["$arg"] ]]; then
+          b="${required_flags[$arg]}"
+          (( b == 1 )) && __usage_error "flag: $arg was repeated"
           required_flags["$arg"]="1"
 
           (( $# == 1 )) && __usage_error "flag $arg requires an argument"
           __args["$arg"]="$2"
           shift 2
-        elif [[ -n "$b_of" ]]; then
-          (( b_of == 1 )) && __usage_error "flag $arg was repeated"
+        elif [[ -v optional_flags["$arg"] ]]; then
+          b="${optional_flags[$arg]}"
+          (( b == 1 )) && __usage_error "flag $arg was repeated"
           optional_flags["$arg"]="1"
 
           (( $# == 1 )) && __usage_error "flag $arg requires an argument"
           __args["$arg"]="$2"
           shift 2
-        elif [[ -n "$b_sw" ]]; then
-          (( b_sw == 1 )) && __usage_error "flag $arg was repeated"
+        elif [[ -v known_switches["$arg"] ]]; then
+          b="${known_switches[$arg]}"
+          (( b == 1 )) && __usage_error "flag $arg was repeated"
           known_switches["$arg"]="1"
           __args["$arg"]="1"
           shift
@@ -174,9 +172,85 @@ __parse_flags() {
     fi
   done
 
-  if (( restore_nounset == 1 )); then
-    set -u
+  unset __usage_error
+}
+
+### Example usage:
+###
+###     ian_parse_flags_subcmd \
+###       add 'PATH [--name=] --verbose' \
+###       remove 'NAME' \
+###       -- "$@"
+###
+###     case "$__subcmd" in
+###       add)
+###         main_add  # `main_add` can access `__args` like usual
+###         ;;
+###       remove)
+###         main_remove
+###         ;;
+###     esac
+###
+__subcmd=""
+ian_parse_flags_subcmd() {
+  declare -A subcmds
+  subcmds_help=()
+  while (( $# > 0 )); do
+    arg="$1"
+    case "$1" in
+      --)
+        shift
+        break
+        ;;
+      -*)
+        echo "BUG: unexpected flag: $arg (in ian_parse_flags_subcmd)"
+        exit 1
+        ;;
+      *)
+        (( $# == 1 )) && ( echo "BUG: subcmd $arg missing description (in ian_parse_flags_subcmd)"; exit 1 )
+        subcmds["$arg"]="$2"
+        subcmds_help+=("  $arg $2")
+        shift 2
+        ;;
+    esac
+  done
+
+  __print_usage() {
+    echo "usage: $0 SUBCMD"
+    echo
+    for help in "${subcmds_help[@]}"; do
+      echo "$help"
+    done
+    echo
+  }
+
+  __usage_error() {
+    __print_usage
+    echo "error: $1"
+    exit 1
+  }
+
+  if (( $# == 0 )); then
+    __usage_error "missing subcommand"
   fi
 
+  if [[ "$1" = -h ]] || [[ "$1" = -help ]] || [[ "$1" = --help ]] || [[ "$1" = help ]]; then
+    __print_usage
+    exit
+  fi
+
+  __subcmd="$1"
+  shift
+
+  if ! [[ -v subcmds[$__subcmd] ]]; then
+    __usage_error "unknown subcommand: $__subcmd"
+  fi
+  subcmd_desc="${subcmds[$__subcmd]}"
+
+  export IAN_PARSE_FLAGS_CMD="$0 $__subcmd"
+  ian_parse_flags "$subcmd_desc" "$@"
+  unset IAN_PARSE_FLAGS_CMD
+
+  unset __print_usage
   unset __usage_error
 }
