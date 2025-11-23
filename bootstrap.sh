fd96edcade98fe7311d8d80822a59fcb4f0eca45
Ian Fisher <ian@iafisher.com>
2025-11-23 10:04:35 -0500

    bootstrap: further improvements

--- bootstrap.sh
+++ bootstrap.sh
@@ -29,7 +29,8 @@ main() {
   echo "==> setting up local 'dotfiles' repo"
   mkdir -p "$dotfiles_dir"
   cd "$dotfiles_dir"
-  git init
+  # `-b master` silences a warning about the default branch.
+  git init -b master
 
   cp "$foundation_dir/assets/gitignore" .gitignore
   git add .gitignore
@@ -45,63 +46,74 @@ main() {
     shellrc="bashrc"
   fi
 
+  # If the system has a ~/.bashrc or ~/.zshrc already, copy it to the dotfiles
+  # repo as bashrc.system or zshrc.system.
+  shellrc_system="$HOME/.$shellrc"
+  if [[ -e "$shellrc_system" ]]; then
+    shellrc_system_local="$dotfiles_dir/${shellrc}.system"
+    echo "WARNING: System has $shellrc file already. Copying to $shellrc_system_local."
+    cp "$shellrc_system" "$shellrc_system_local"
+    git add "$shellrc_system_local"
+    echo "source $shellrc_system_local" >> "$shellrc"
+  fi
+
   (cat << EOF
-source "$HOME/.ian/foundation/shell/env"
-" This file contains machine-specific configuration.
+source $HOME/.ian/foundation/shell/env
+# This file contains machine-specific configuration.
 EOF
-) > "$shellrc"
+) >> "$shellrc"
   git add "$shellrc"
 
   (cat << EOF
 source $HOME/.ian/foundation/vimrc
 " This file contains machine-specific configuration.
 EOF
-) > vimrc
+) >> vimrc
   git add vimrc
 
+  git_name="Ian Fisher"
+  git_email="ian@iafisher.com"
   (cat << EOF
+[user]
+	name = $git_name
+	email = $git_email
+
 [include]
-  path = $HOME/.ian/foundation/gitconfig
+	path = $HOME/.ian/foundation/gitconfig
 EOF
-) > gitconfig
+) >> gitconfig
   git add gitconfig
 
-  git config --global user.name 'Ian Fisher'
-  git config --global user.email 'ian@iafisher.com'
-  git commit -m "initial commit" --author 'Ian Fisher <ian@iafisher.com>'
+  export GIT_AUTHOR_NAME="$git_name"
+  export GIT_AUTHOR_EMAIL="$git_email"
+  export GIT_COMMITTER_NAME="$git_name"
+  export GIT_COMMITTER_EMAIL="$git_email"
+  git commit -m "initial commit"
 
   echo "==> configuring shell"
-  f="$HOME/.$shellrc"
-  if [[ -e "$f" ]]; then
-    echo "WARNING: will not overwrite $f. You must either:"
-    echo "  - Run 'ln -sf $dotfiles_dir/$shellrc $f'"
-    echo "  - Add 'source $dotfiles_dir/$shellrc' to the end of the file."
-  else
-    ln -s "$dotfiles_dir/$shellrc" "$f"
-  fi
+  make_symlink "$HOME/.$shellrc" "$dotfiles_dir/$shellrc"
 
   echo "==> configuring vim"
-  f="$HOME/.vimrc"
-  if [[ -e "$f" ]]; then
-    echo "WARNING: will not overwrite $f. You must either:"
-    echo "  - Run 'ln -sf $dotfiles_dir/vimrc $f'"
-    echo "  - Add 'source $dotfiles_dir/vimrc' to the end of the file."
-  else
-    ln -s "$dotfiles_dir/vimrc" "$f"
-  fi
+  make_symlink "$HOME/.vimrc" "$dotfiles_dir/vimrc"
 
   echo "==> configuring git"
-  f="$HOME/.gitconfig"
-  if [[ -e "$f" ]]; then
-    echo "WARNING: will not overwrite $f. You must either:"
-    echo "  - Run 'ln -sf $dotfiles_dir/gitconfig $f'"
-    echo "  - Add 'path = $dotfiles_dir/gitconfig' to an '[include]' block in the file."
-  else
-    ln -s "$dotfiles_dir/gitconfig" "$f"
-  fi
+  make_symlink "$HOME/.gitconfig" "$dotfiles_dir/gitconfig"
 
   echo
   echo "==> done"
+  source "$HOME/.$shellrc"
+}
+
+make_symlink() {
+  target_path="$1"
+  src_path="$2"
+
+  if [[ -e "$target_path" ]]; then
+    backup="${target_path}.bak"
+    echo "WARNING: overwriting ${target_path}. Backup saved to ${backup}."
+    cp "$target_path" "$backup"
+  fi
+  ln -sf "$src_path" "$target_path"
 }
 
 main "$@"
